465e505b704f93da8706508a9ba03bd32e623329
ozayerislam-dsi <49142817+ozayerislam-dsi@users.noreply.github.com>
2022-02-09 08:53:24 +0600

    Source Codes for master design and testbench

---
+++ spi_master.v
@@ -0,0 +1,111 @@
+`timescale 1ns / 1ps
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Ozayer Islam
+//
+// Create Date:   04/02/2022
+// Design Name:   spi_master
+// Module Name:   spi_master.v
+// Project Name:  SPI
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// 
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+module spi_master(
+    input wire[7:0] in_data,
+    input wire clk,
+    input wire wr,
+    input wire rd,
+    input wire cs, // active low
+    output reg[7:0] out_data,
+    output mosi,
+    input miso,
+    inout sclk
+    );
+    
+    // --------define internal register and buffer--------
+    // output buffer stage
+    reg sclk_buf = 0; // SCLK buffer
+    reg mosi_buf = 0; // MOSI Buffer
+ 
+    reg busy = 0; // when set to 0, READ/WRITE state, otherwise Shift/Buffer state 
+    reg[7:0] in_buf = 0; // shift register, Buffer that saves the input data For MOSI
+    reg[7:0] out_buf = 0; // shift register, Buffer that saves the output data For MISO
+
+    reg[4:0] cnt = 0; // Variable to keep count of sclk 
+    // --------------------------------------------------
+
+    // the port of module links internal buffer
+    assign sclk = sclk_buf;
+    assign mosi = mosi_buf;
+
+    //sclk positive edge read data into out-shift register from miso , implement read operation
+    always @(posedge sclk_buf) begin
+        out_buf[0] <= miso;
+        out_buf <= out_buf << 1;
+    end 
+
+    // read data (combinatorial logic that level sensitive , detect all input)
+    always @(cs or wr or rd or out_buf or busy) begin
+        out_data = 8'bx;
+        if (!cs && rd) begin
+            out_data = out_buf;
+        end
+    end
+    
+    // sclk negetive edge write data to mosi
+    always @(posedge clk) begin
+        if (!busy) begin // idle state load data into send buffer
+            if(!cs && wr) begin // When cs is low and wr is high
+                  in_buf <= in_data; // Take Input data to Internal Buffer
+                  busy <= 1; // Set Busy Signal High
+                  cnt <= 0;
+            end
+            else if(!cs && rd) begin
+                busy <= 1;
+                cnt <= 0;
+            end
+        end
+        else begin // when 8-bits data is written into buffer , send it bit by bit in MOSI
+                if (cnt % 2 == 0) begin // when sclk_buf is negitive , shift data into mosi buffer
+                    mosi_buf <= in_buf[7];
+                    in_buf <= in_buf << 1;
+                    $display("Reached at cnt mod 2 logic, mosi_buf = %h, in_buf = %h", mosi_buf, in_buf);
+                end 
+                else begin
+                    mosi_buf <= mosi_buf; // Do nothing
+                    
+                end
+
+                if (cnt > 0 && cnt < 17) begin
+                    sclk_buf <= ~sclk_buf; // Toggle the sclk 16 times to send 8 bits
+                end
+
+                // 8-bits had sent over , spi regain idle
+                if (cnt >= 17) begin 
+                    cnt <= 0;
+                    busy <= 0;
+                end
+                else begin
+                    cnt <= cnt; // Do Nothing
+                    busy <= busy; // Do Nothing
+                end
+
+                cnt <= cnt + 1; // Keep Counting
+        end 
+    end
+
+
+    
+
+
+endmodule
---
+++ testbench.sv
@@ -0,0 +1,108 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Ozayer Islam
+//
+// Create Date:   04/02/2022
+// Design Name:   spi_master
+// Module Name:   spi_master_tb.v
+// Project Name:  SPI
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// 
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module spi_master_tb;
+
+    // Inputs
+    reg [7:0] in_data;
+    reg clk;
+    reg wr;
+    reg rd;
+    reg cs;
+    reg miso;
+
+    // Outputs
+    wire [7:0] out_data;
+
+    // Bidirs
+    wire mosi;
+    wire sclk;
+
+    // Instantiate the Unit Under Test (UUT)
+    spi_master uut (
+        .in_data(in_data), 
+        .clk(clk), 
+        .wr(wr), 
+        .rd(rd), 
+        .cs(cs), 
+        .out_data(out_data), 
+        .mosi(mosi), 
+        .miso(miso), 
+        .sclk(sclk)
+    );
+
+    initial begin
+        // Initialize Inputs
+        in_data = 0;
+        clk = 0;
+        wr = 0;
+        rd = 0;
+        cs = 1;
+        miso = 0;
+
+        // set clk_div , and out by out_data
+        #40;
+        in_data = 8'haa;
+        wr = 1;
+        cs = 0;
+        
+        // write data 
+        #20 ;
+        wr = 0;
+        cs = 1;
+
+        #360 ;
+        wr = 1;
+        cs = 0;
+        in_data = 8'h91;
+
+        #20 ;
+        wr = 0;
+        cs = 1;
+        
+        #360 ;
+        wr = 1;
+        cs = 0;
+        in_data = 8'hf0;
+
+        #20 ;
+        wr = 0;
+        cs = 1;
+
+        #360 ;
+        wr = 1;
+        cs = 0;
+        in_data = 8'h12;
+
+        #20 ;
+        wr = 0;
+        cs = 1;
+    end
+
+    // define clock
+    initial begin
+        clk = 0;
+        forever #10 clk = ~clk;
+    end
+endmodule
